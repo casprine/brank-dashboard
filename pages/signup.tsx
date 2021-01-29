56e6ff1479eed39d61b63e8698a71d6753724efd
Casprine Assempah <casprine.001@gmail.com>
2021-01-29 23:42:56 +0000

    gs

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 
 import { Button, Input } from 'components/form';
-import { Layout, Pattern } from 'components/common';
+import { Layout } from 'components/common';
 
 const SignupPage = () => {
   return (
---
+++ theme/presets.ts
@@ -0,0 +1,9 @@
+import { CSSObject } from '@emotion/core';
+
+const rounded: CSSObject = { borderRadius: '50%' };
+
+const presets = {
+  rounded: rounded,
+};
+
+export default presets;
