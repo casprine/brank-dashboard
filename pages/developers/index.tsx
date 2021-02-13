875d19b749e5544e1cc4d010f4f64949453c919e
Casprine Assempah <casprine.001@gmail.com>
2021-02-13 01:13:35 +0000

    add coming soon pages

--- pages/developers/index.tsx
+++ pages/developers/index.tsx
@@ -1,8 +1,13 @@
 import { DashboardLayout } from 'components/common';
+import ComingSoon from 'components/comingSoon/comingSoon';
+
 const Developers = () => {
   return (
     <DashboardLayout>
-      <div>Developers page</div>
+      <ComingSoon
+        icon={{ name: 'external-link-square' }}
+        description="Add a layer of intelligence in your spending process by creating virtual cards that help you categorize spending as well as capture receipts associated with each transaction."
+      />{' '}
     </DashboardLayout>
   );
 };
--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -1,8 +1,13 @@
 import { DashboardLayout } from 'components/common';
+import ComingSoon from 'components/comingSoon/comingSoon';
+
 const Products = () => {
   return (
     <DashboardLayout>
-      <div>Products page</div>
+      <ComingSoon
+        icon={{ name: 'box' }}
+        description="Add a layer of intelligence in your spending process by creating virtual cards that help you categorize spending as well as capture receipts associated with each transaction."
+      />{' '}
     </DashboardLayout>
   );
 };
