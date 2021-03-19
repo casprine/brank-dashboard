804590fb16f87e008ce94b5976f2589fef95bbf4
Casprine Assempah <casprine.001@gmail.com>
2021-03-19 09:31:23 +0000

    updated base urls

--- utils/hermes.ts
+++ utils/hermes.ts
@@ -3,7 +3,7 @@ import Cookies from 'js-cookie';
 
 // our network system powered by the Olympian of god of transport
 
-const baseUrl: string = 'https://brank-core.herokuapp.com';
+const baseUrl: string = 'https://api.withbrank.com';
 
 interface IHermes {
   data?: any;
